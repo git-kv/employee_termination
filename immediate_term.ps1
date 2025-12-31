3193b5e3823829e350645292815fdeaf8111f633
Keith <8584184+git-kv@users.noreply.github.com>
2025-12-31 10:31:28 -0600

    fixed looping for real

--- immediate_term.ps1
+++ immediate_term.ps1
@@ -3,7 +3,7 @@
 $root_path = "\\eocservices\apps$\programs\scripts\separation\immediate_account_disablement\"
 $smtp_server = "exnode2.dartadvantage.com"
 $from = "ImmediateTerm@dart.net"
-
+$Global:disable_account = $false
 
 while ($true) {
     $ShouldSync = Test-Path "$root_path*"
@@ -17,21 +17,18 @@ while ($true) {
             $default_subject = "Account Disabled $user_to_be_disabled"
             $default_body = "$user_to_be_disabled's account has been disabled."
             $default_recipients = "kvoelker@dart.net"
-            $disable_account = $false
+            $disable_account = $true
 
             Get-ADPrincipalGroupMembership $user_to_be_disabled | Select-Object Name | ForEach-Object {
                 if (($_.Name -eq "T2_Admins") -or ($_.Name -like "T2_VIP") -or ($_.Name -like "T1*") -or ($_.Name -like "T0*") -or ($_.Name -like "*Domain Admins")) {
                     Send-MailMessage -Subject $vip_subject -Body $vip_body -To $vip_recipients -From $from -SmtpServer $smtp_server
                     $disable_account = $false
                 }
-                else {
-                    $disable_account = $true
-                }
             }
 
             if ($disable_account) {
                 Disable-ADAccount -Identity $user_to_be_disabled
-                New-Item "\\eocservices\apps$\programs\scripts\active_directory\needsync\sync_plz"
+                New-Item "\\eocservices\apps$\programs\scripts\active_directory\needsync\sync_plz_$user_to_be_disabled"
                 Send-MailMessage -Subject $default_subject -Body $default_body -To $default_recipients -From $from -SmtpServer $smtp_server
             }
 
